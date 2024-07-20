7108f467a16d6dda01fbc9fddb36cce2ad2bb28c
Yohei-Fujii110 <yfujii0110@gmail.com>
2024-07-20 18:42:12 +0900

    クエリビルダを使ってデータを取得しよう_初回コミット

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller {
     public function index() {
-        return view('posts/index');
+        // postsテーブルからすべてのデータを取得し、変数$postsに代入
+        $posts = DB::table('posts')->get();
+
+        // 変数$postsをpost\index.blade.phpに返す
+        return view('posts.index', compact('posts'));
     }
 }
